91428be16f1df017e521a75a20e59264557b8eba
Lena Gagnon <lgagnon22@hotmail.com>
2023-12-14 12:59:03 -0500

    couldn't get anu=ything to work and figured out that categories don't show up on ingredients#index if a user_ingredient is in one of those categories! So let's change branch and try to fix

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -7,10 +7,25 @@ class IngredientsController < ApplicationController
       @ingredients = Ingredient.all
     end
     @user_ingredient_ids = UserIngredient.where(user: current_user).pluck(:ingredient_id)
-    ingredients = Ingredient.find(@user_ingredient_ids)
+    # ingredients = Ingredient.find(@user_ingredient_ids)
 
-    category_ids = ingredients.pluck(:category_id)
-    @categories = Category.where.not(id: category_ids)
+    # category_ids = ingredients.pluck(:category_id)
+    # @categories = Category.where.not(id: category_ids)
+    @categories = Category.all
   end
 
 end
+
+# if params[:query].present?
+#   @ingredients = Ingredient.search_ingredient_in_db(params[:query])
+#   raise
+#   category_ids = @ingredients.pluck(:category_id)
+#   @categories = Category.find(category_ids)
+#   # @categories should take Category that are included in ingredient category_id only.
+# else
+#   @ingredients = Ingredient.all
+
+#   @user_ingredient_ids = UserIngredient.where(user: current_user).pluck(:ingredient_id)
+#   ingredients = Ingredient.find(@user_ingredient_ids)
+#   category_ids = ingredients.pluck(:category_id)
+#   @categories = Category.where.not(id: category_ids)
